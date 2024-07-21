632fa003ce9a440dfd9cd6f07ba1129b52842fff
rythmn1111 <rythmnmagnani0@gmail.com>
2024-07-21 18:54:25 +0530

    menu closing when clicking anywhere else other than menu

--- src/app_directory/app_directory.tsx
+++ src/app_directory/app_directory.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 function directory(){
@@ -7,13 +7,28 @@ function directory(){
 
 export default function AppDirectory(){
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
 
-    const toggleMenu = () => {
+    const toggleMenu = (e: React.MouseEvent) => {
+        // e.stopPropagation();
         setIsMenuOpen(!isMenuOpen);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className="app-directory-container">
+        <div className="app-directory-container" ref={menuRef}>
         <Image src="/app_directory.svg" alt="Logo" width={40} height={40} style={{}} onClick={toggleMenu} className="app-directory" />
         {isMenuOpen && (<div className="app-directory-menu">
             <div>Menu Item 1</div>
